79f238fe1ad41f4a51126929ff1430b9f0e49453
ajdevaux <32491625+ajdevaux@users.noreply.github.com>
2021-12-24 18:06:27 +0100

    tweaks and credits

--- variant_graphing.py
+++ variant_graphing.py
@@ -18,12 +18,22 @@ wwtp_list=["ARA Werdhölzli","STEP Aire","ARA Altenrhein","ARA Chur","CDA Lugano
 plot_list = []
 
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+
+whisker_props = {
+    "base":"sample_date",
+    "upper":"upper_ci",
+    "lower":"lower_ci",
+    "line_width":2,
+    "line_cap":"round",
+    "upper_head":None,
+    "lower_head":None
+}
 for wwtp in wwtp_list:
     subplot_df = variant_df[variant_df["wwtp"]==wwtp].copy()
 
     delta_df = subplot_df[subplot_df["target_variant"] ==  "S:L452R (Delta)"]
     omicron_df = subplot_df[subplot_df["target_variant"] ==  '69-70del (Alpha, Omicron)']
-    # colormap = {'S:L452R (Delta)': 'green', '69-70del (Alpha, Omicron)': 'cyan'}
+    # colormap = {'S:L452R (Delta)': 'lightgreen', '69-70del (Alpha, Omicron)': 'cyan'}
     # subplot_df['colors'] = [colormap[x] for x in zh_df['target_variant']]
 
     p = figure(
@@ -32,44 +42,45 @@ for wwtp in wwtp_list:
         x_axis_type="datetime",
         title=f"{wwtp}",
         tools=TOOLS,
-        x_range=(pd.to_datetime("05-01-2021"), pd.to_datetime(datetime.now().date())),
+        x_range=(pd.to_datetime("08-01-2021"), pd.to_datetime(datetime.now().date())),
         y_range=(-5, 105)
 
     )
+    p.title.text_font_size = '16pt'
 
     p.xaxis.axis_label = 'Sample Date'
+    p.xaxis.axis_label_text_font_size = '10pt'
+
     p.yaxis.axis_label = 'Percent hCoV-2019 Mutation'
+    p.yaxis.axis_label_text_font_size = '10pt'
 
     delta_src = ColumnDataSource(delta_df)
     omi_src = ColumnDataSource(omicron_df)
 
-    p.circle(x="sample_date", y="PercMutation", color="lightgreen", size=6, line_alpha=0,legend_label="Delta (B.1.617.2)",source=delta_src)
-    p.line(x="sample_date", y="PercMutation", color="lightgreen", line_width=3,source=delta_src)
-    p.circle(x="sample_date", y="PercMutation", color="blue", size=6, line_alpha=0,legend_label="Omicron (B.1.1.529)",source=omi_src)
-    p.line(x="sample_date", y="PercMutation", color="blue", line_width=3,source=omi_src)
+
+    p.line(x="sample_date", y="PercMutation", color="lightgreen", line_width=3,legend_label="Delta (B.1.617.2)",source=delta_src)
+    p.circle(x="sample_date", y="PercMutation", color="green", size=6, line_alpha=0,source=delta_src)
+    p.line(x="sample_date", y="PercMutation", color="orange", line_width=3,legend_label="Omicron (B.1.1.529)",source=omi_src)
+    p.circle(x="sample_date", y="PercMutation", color="darkorange", size=6, line_alpha=0,source=omi_src)
+
 
     p.add_layout(
         Whisker(
             source=delta_src,
-            base="sample_date",
-            upper="upper_ci",
-            lower="lower_ci",
             line_color="green",
-            line_width=2)
+            **whisker_props
+        )
     )
     p.add_layout(
         Whisker(
             source=omi_src,
-            base="sample_date",
-            upper="upper_ci",
-            lower="lower_ci",
-            line_color="cyan",
-            line_width=2,
-            line_cap="round")
+            line_color="darkorange",
+            **whisker_props
+        )
     )
 
-    p.legend.location = "top_left"
-    p.legend.click_policy="hide"
+    p.legend.location = "bottom_left"
+    # p.legend.click_policy="hide"
 
     plot_list.append(p)
 # p.circle('petal_length', 'petal_width', color='colors',
@@ -95,6 +106,20 @@ logo1 = Div(text=
     <img src='images/logo_epfl_white.png' style="width:169px;height:49;border:10px solid black;">
     """
 )
+div2 = Div(text=
+    """
+    <p style="color:white;">This data made possible by the tireless work of:</p>
+    <ul style="color:white;">
+        <li>Lea Caduff</li>
+        <li>Pravin Ganesanandamoorthy</li>
+        <li>Charlie Gan</li>
+        <li>Johannes Rusch</li>        
+        <li>Franszska Böni</li>
+        <li>A.J. Devaux (<a href = "mailto:alexander.devaux@eawag.ch">Webmaster</a>)</li>
+        <li>And the rest of the Eawag & EPFL Team</li>
+    </ul>
+    """
+)
 header = row(div1,logo1, sizing_mode="scale_width")
 grid = gridplot(plot_list,ncols=2,toolbar_location="left")
-show(column(header,grid),background="black")
+show(column(header,grid,div2),background="black")
