29fda2d82afa5b4025c61593c54ba1c2e8965c0f
ajdevaux <32491625+ajdevaux@users.noreply.github.com>
2021-12-24 16:57:57 +0100

    uploading first graphs

---
+++ variant_graphing.py
@@ -0,0 +1,100 @@
+from bokeh.plotting import figure, show, output_file, ColumnDataSource
+from bokeh.models import HoverTool, Whisker, Div
+from bokeh.layouts import row,column, gridplot
+from bokeh.io import curdoc
+from datetime import datetime
+# from skimage import io as skio
+import pandas as pd
+import numpy as np
+
+
+data = "data/variant_data.csv"
+variant_df = pd.read_csv(data)
+variant_df.set_index("sample_date",inplace=True)
+variant_df.index = pd.to_datetime(variant_df.index)
+
+wwtp_list=["ARA Werdhölzli","STEP Aire","ARA Altenrhein","ARA Chur","CDA Lugano","ARA Sensetal"]
+
+plot_list = []
+
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+for wwtp in wwtp_list:
+    subplot_df = variant_df[variant_df["wwtp"]==wwtp].copy()
+
+    delta_df = subplot_df[subplot_df["target_variant"] ==  "S:L452R (Delta)"]
+    omicron_df = subplot_df[subplot_df["target_variant"] ==  '69-70del (Alpha, Omicron)']
+    # colormap = {'S:L452R (Delta)': 'green', '69-70del (Alpha, Omicron)': 'cyan'}
+    # subplot_df['colors'] = [colormap[x] for x in zh_df['target_variant']]
+
+    p = figure(
+        plot_height=400,
+        plot_width=400,
+        x_axis_type="datetime",
+        title=f"{wwtp}",
+        tools=TOOLS,
+        x_range=(pd.to_datetime("05-01-2021"), pd.to_datetime(datetime.now().date())),
+        y_range=(-5, 105)
+
+    )
+
+    p.xaxis.axis_label = 'Sample Date'
+    p.yaxis.axis_label = 'Percent hCoV-2019 Mutation'
+
+    delta_src = ColumnDataSource(delta_df)
+    omi_src = ColumnDataSource(omicron_df)
+
+    p.circle(x="sample_date", y="PercMutation", color="lightgreen", size=6, line_alpha=0,legend_label="Delta (B.1.6172)",source=delta_src)
+    p.line(x="sample_date", y="PercMutation", color="lightgreen", line_width=3,source=delta_src)
+    p.circle(x="sample_date", y="PercMutation", color="blue", size=6, line_alpha=0,legend_label="Omicron (B.1.1.529)",source=omi_src)
+    p.line(x="sample_date", y="PercMutation", color="blue", line_width=3,source=omi_src)
+
+    p.add_layout(
+        Whisker(
+            source=delta_src,
+            base="sample_date",
+            upper="upper_ci",
+            lower="lower_ci",
+            line_color="green",
+            line_width=2)
+    )
+    p.add_layout(
+        Whisker(
+            source=omi_src,
+            base="sample_date",
+            upper="upper_ci",
+            lower="lower_ci",
+            line_color="cyan",
+            line_width=2,
+            line_cap="round")
+    )
+
+    p.legend.location = "top_left"
+    p.legend.click_policy="hide"
+
+    plot_list.append(p)
+# p.circle('petal_length', 'petal_width', color='colors',
+#          fill_alpha=0.2, size=10, source=ColumnDataSource(flowers))
+# data['y_fixed'] = [1200 - val for val in data.y]
+# x = zh_df.sample_date
+# y = zh_df.PercMutation
+# pc = particle_df.pc
+curdoc().theme = 'dark_minimal'
+output_file('variant_monitoring.html')
+div1 = Div(text=
+    """
+    <style>
+        body { background: #0F0F0F; }
+    </style>
+    <h1 style="color:white;">Coronavirus hCoV-2019 Variant Prevalence in Swiss Wastewater</h1>
+    """
+)
+logo1 = Div(text=
+    """
+    <img src='images/ealogo-white.png' style="width:200px;height:42.5;border:10px solid black;">
+
+    <img src='images/logo_epfl_white.png' style="width:169px;height:49;border:10px solid black;">
+    """
+)
+header = row(div1,logo1, sizing_mode="scale_width")
+grid = gridplot(plot_list,ncols=3,toolbar_location="left")
+show(column(header,grid),background="black")
