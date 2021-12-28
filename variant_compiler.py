9e29b3fe4ac043e971ff719b02cece92beb9c49d
ajdevaux <32491625+ajdevaux@users.noreply.github.com>
2021-12-28 01:12:33 +0100

    code cleanup

--- variant_compiler.py
+++ variant_compiler.py
@@ -1,28 +1,128 @@
 #!/usr/bin/env python3
 import matplotlib as mpl
 mpl.use("Qt5Agg")
-import matplotlib.pyplot as plt
-import gspread
-import gspread_dataframe as gsdf
+# import matplotlib.pyplot as plt
 
 import os
-import pandas as pd
-import numpy as np
 import glob
 import re
+import pandas as pd
+import numpy as np
 import scipy.optimize as spo
 import scipy.stats as sps
 import seaborn as sns
-
-# from datetime import datetime
-from funcs.stritfuncs import get_q_dir, get_wwtp_df, sample_name_parser, excel_cols
+import gspread
+import gspread_dataframe as gsdf
 
 pd.set_option("display.width", 1000)
 pd.options.display.max_rows = 2000
 pd.options.display.max_columns = 15
 
-gc = gspread.service_account(filename="cowwid-wave5-e31fa2771e32.json")
 
+
+##------------------------------------------------------------------------------------------------##
+def sample_name_parser(string_to_parse, new_dict):
+    j = 1
+    sample_types = ("ntc","neg","pos","post","std","extcon","extc","ext","ec","exc",
+                    "ef","sand","saliva","stock","pex")
+
+    while j <= len(string_to_parse):
+        try:
+            substr = string_to_parse[j]
+
+            substr = substr.lower().strip()
+
+            if (len(substr) <= 2) & (substr.isdigit()):
+                if len(substr) == 1: substr = substr.zfill(2)
+
+                wwtp = substr
+                new_dict["wwtp"] = wwtp
+
+            elif substr in sample_types:
+                if substr in sample_types[0:2]:
+                    substr = "ntc"
+                    new_dict["wwtp"] = substr
+                elif substr in sample_types[2:5]:
+                    substr = "pos"
+                    new_dict["wwtp"] = substr
+                elif substr in sample_types[5:10]:
+                    substr = "exc"
+                    new_dict["wwtp"] = substr
+
+                new_dict["sample_type"] = substr
+
+            elif (substr in ("2020","2021")) & (len(substr) == 4):
+                year = substr
+                month = string_to_parse[j+1]
+                dayrep = string_to_parse[j+2]
+
+                if not dayrep.isdigit():
+                    day = 00
+                    j -= 1
+
+                if dayrep[-1].upper() in ("A","B","C","D","I"):
+                    day = dayrep[:-1]
+                    rep = dayrep[-1]
+                    new_dict["replicate"] = rep.upper()
+
+                elif "to" in dayrep:
+                    dayrep = dayrep.split(" ")
+
+                    if len(dayrep) > 1:
+                        day = dayrep[0]
+                        dil = dayrep[1].split("to")[-1]
+                    else:
+                        day = dayrep[:2]
+                        dil = dayrep.split("to")[-1]
+
+                    new_dict["dilution"] = f"{dil}x"
+                else:
+                    day = string_to_parse [j+2]
+
+                sample_date = f"{year}-{month}-{day}"
+                new_dict["sample_date"] = sample_date
+                j += 2
+                pass
+            elif (len(substr) == 1) & (substr.upper() in ("Q","W","Y","Z")):
+                pcr_id = substr.upper()
+                new_dict["PCR_ID"] = pcr_id
+
+            elif substr in "spiked":
+                new_dict["spiked"] = True
+            elif (len(substr) == 1) & (substr.upper() in ("A","B","C","D","I")):
+                new_dict["replicate"] = substr.upper()
+            elif substr in ("ultraplex","up"):
+                new_dict["pcr_mix"] = substr
+            elif substr in ("qsv"):
+                new_dict["pcr_mix"] = "qscript_virus"
+            elif substr in ("BSA2ug"):
+                new_dict["pcr_mix"] = new_dict["pcr_mix"] + "_2ugBSA"
+            elif (substr.isalnum() & substr.endswith("x")):
+                new_dict["dilution"] = substr
+            elif substr in ("stericup","zymo"):
+                new_dict["filt_protocol"] = substr
+            elif substr in ("pmga","pmg2"):
+                pmga_version = substr[-1]
+                new_dict["volume_mL"] = 40
+                new_dict["replicate"] = "C"
+                if pmga_version == "a":
+                    elution_offset_dil = int(new_dict["dilution"][:-1])*0.5
+                    new_dict["dilution"] = f"{elution_offset_dil}x"
+                # elif pmga_version == "2":
+                #     new_dict["replicate"] = "C"
+            elif substr in ("sendai","mhv","iav","ibv"):
+                # new_dict["target"] = substr
+                new_dict["sample_type"] = f"{substr}_stock"
+
+            else:
+                new_dict[f"unk_{j}"] = substr
+
+            j += 1
+
+        except IndexError:
+            break
+
+    return new_dict
 ##------------------------------------------------------------------------------------------------##
 def _parse_assay_counts(droplet_cts):
     """
@@ -166,45 +266,79 @@ def loglik_trinom_prof(droplet_cts,ratio):
         # print(trinomial)
         return trinomial
 ##------------------------------------------------------------------------------------------------##
-def lower_e(droplet_cts, level=0.95):
-    """
-    Function translated from R, orig. author David Dreifuss:
-
-    lower_e <- function(x, level=0.95){
-      if(is.nan(r_hat(x))){
-        return(NaN)
-      }else if(r_hat(x) == 0){
-        return(0)
-      }else{
-        uniroot(function(y){2*(loglik_trinom_prof(x,y) - loglik_trinom_prof(x,r_hat(x))) + qchisq(level, 1)},
-                interval=c(0, r_hat(x)))$root
-      }
-    }"""
-    r_hat_val = r_hat(droplet_cts)
-    if np.isnan(r_hat_val):
-        print(np.nan)
-        return np.nan
-    elif r_hat_val == 0:
-        print(0)
-        return 0
-    else:
-        lp_rhat = loglik_trinom_prof(droplet_cts, r_hat_val)
-        chi_sq = sps.chi2.ppf(level, 1)
-
-        try:
-            root = spo.brentq(lambda r: 2*(loglik_trinom_prof(droplet_cts,r) - lp_rhat) + chi_sq,
-                a=0.00001,
-                b=r_hat_val
-            )
-            return root
-        except ValueError:
-            return np.nan
-
-
+# def lower_e(droplet_cts, level=0.95):
+#     """
+#     Function translated from R, orig. author David Dreifuss:
+#
+#     lower_e <- function(x, level=0.95){
+#       if(is.nan(r_hat(x))){
+#         return(NaN)
+#       }else if(r_hat(x) == 0){
+#         return(0)
+#       }else{
+#         uniroot(function(y){2*(loglik_trinom_prof(x,y) - loglik_trinom_prof(x,r_hat(x))) + qchisq(level, 1)},
+#                 interval=c(0, r_hat(x)))$root
+#       }
+#     }"""
+#     r_hat_val = r_hat(droplet_cts)
+#     if np.isnan(r_hat_val):
+#         print(np.nan)
+#         return np.nan
+#     elif r_hat_val == 0:
+#         print(0)
+#         return 0
+#     else:
+#         lp_rhat = loglik_trinom_prof(droplet_cts, r_hat_val)
+#         chi_sq = sps.chi2.ppf(level, 1)
+#
+#         try:
+#             root = spo.brentq(lambda r: 2*(loglik_trinom_prof(droplet_cts,r) - lp_rhat) + chi_sq,
+#                 a=0.00001, ##Should be zero
+#                 b=r_hat_val
+#             )
+#             return root
+#         except ValueError:
+#             return np.nan
 ##------------------------------------------------------------------------------------------------##
-def upper_e(droplet_cts, level=0.95):
+# def upper_e(droplet_cts, level=0.95):
+#     """
+#     Function translated from R, orig. author David Dreifuss:
+#
+#     upper_e <- function(x, level=0.95){
+#       if (is.nan(r_hat(x))){
+#         return(NaN)
+#       }else if (r_hat(x) == 1){
+#         return(1)
+#       }else{
+#         uniroot(function(y){2*(loglik_trinom_prof(x,y) - loglik_trinom_prof(x,r_hat(x))) + qchisq(level, 1)},
+#                 interval=c(r_hat(x), 1), f.lower = qchisq(level, 1))$root
+#       }
+#     }
+#     """
+#     r_hat_val = r_hat(droplet_cts)
+#     if np.isnan(r_hat_val):
+#         # print(np.nan)
+#         return np.nan
+#     elif r_hat_val == 1:
+#         # print(1)
+#         return 1
+#     else:
+#
+#         lp_rhat = loglik_trinom_prof(droplet_cts, r_hat_val)
+#         chi_sq = sps.chi2.ppf(level, 1)
+#
+#         try:
+#             root = spo.brentq(lambda r: 2*(loglik_trinom_prof(droplet_cts,r) - lp_rhat) + chi_sq,
+#                 a=r_hat_val,
+#                 b=0.9999
+#             )
+#             return root
+#         except ValueError:
+#             return np.nan
+##------------------------------------------------------------------------------------------------##
+def calc_error(droplet_cts, mode, level=0.95):
     """
-    Function translated from R, orig. author David Dreifuss:
+    Function derived from R, orig. author David Dreifuss:
 
     upper_e <- function(x, level=0.95){
       if (is.nan(r_hat(x))){
@@ -216,35 +350,55 @@ def upper_e(droplet_cts, level=0.95):
                 interval=c(r_hat(x), 1), f.lower = qchisq(level, 1))$root
       }
     }
-    """
-    r_hat_val = r_hat(droplet_cts)
-    if np.isnan(r_hat_val):
-        print(np.nan)
-        return np.nan
-    elif r_hat_val == 1:
-        # print(1)
-        return 1
-    else:
 
+    lower_e <- function(x, level=0.95){
+      if(is.nan(r_hat(x))){
+        return(NaN)
+      }else if(r_hat(x) == 0){
+        return(0)
+      }else{
+        uniroot(function(y){2*(loglik_trinom_prof(x,y) - loglik_trinom_prof(x,r_hat(x))) + qchisq(level, 1)},
+                interval=c(0, r_hat(x)))$root
+      }
+    }"""
+
+    r_hat_val = r_hat(droplet_cts)
+
+    if mode == "lower":
+        bound = 0
+        a0 = 0.00001 ##Should be zero
+        b0 = r_hat_val
+    elif mode == "upper":
+        bound = 1
+        a0 = r_hat_val
+        b0 = 0.99999 ##Should be one
+    else:
+        return ValueError
+
+    if np.isnan(r_hat_val):
+        return np.nan
+
+    elif r_hat_val == bound:
+        return bound
+
+    else:
         lp_rhat = loglik_trinom_prof(droplet_cts, r_hat_val)
         chi_sq = sps.chi2.ppf(level, 1)
 
         try:
-            root = spo.brentq(lambda r: 2*(loglik_trinom_prof(droplet_cts,r) - lp_rhat) + chi_sq,
-                a=r_hat_val,
-                b=0.9999
+            root = spo.brentq(
+                lambda r: 2*(loglik_trinom_prof(droplet_cts,r) - lp_rhat) + chi_sq,
+                a=a0, b=b0
             )
             return root
+
         except ValueError:
-            return np.nan
-
-
+            return 1
 ##------------------------------------------------------------------------------------------------##
 if __name__ == '__main__':
 
-    # q_dir = get_q_dir()
-
-    wwtp_df = get_wwtp_df("data/wwtp_info.csv")
+    gc = gspread.service_account(filename="cowwid-wave5-e31fa2771e32.json")
+    wwtp_df = pd.read_csv("data/wwtp_info.csv")
 
     os.chdir("/Volumes/PCR_Cowwid/01_dPCR_data/01_Stilla/NCX_CowwidVariants/04_Delta_Monitoring_Excel_new")
     excel_list = sorted(glob.glob("**/[!~]*.xlsx"))
@@ -328,12 +482,10 @@ if __name__ == '__main__':
 
     variant_df.drop(columns=variant_df.filter(regex="_NegativeDroplets$").columns, inplace=True)
 
-    with pd.ExcelWriter("VariantResults.xlsx", date_format="YYYY-MM-DD", engine="openpyxl") as writer:
-        variant_df.to_excel(writer, sheet_name="Results", index=True)
+    # with pd.ExcelWriter("VariantResults.xlsx", date_format="YYYY-MM-DD", engine="openpyxl") as writer:
+    #     variant_df.to_excel(writer, sheet_name="Results", index=True)
 
-
-
-##Upload to Google_sheet
+    ##Upload to Google_sheet
 
     droplet_cols = [
         "wwtp","sample_date","target_variant",
@@ -363,113 +515,13 @@ if __name__ == '__main__':
     final_variant_df["NegativeDroplets"] = calc_df.apply(calc_neg_droplets,axis=1)
     final_variant_df["PercMutation"] = calc_df.apply(calc_perc_mutation,axis=1)
     final_variant_df["r̂"] = calc_df.apply(r_hat,axis=1)
-    # final_variant_df["LogLikelihoodTrinomProf_R=.99"] = calc_df.apply(loglik_trinom_prof,axis=1,ratio=1)
-    final_variant_df["lower_ci"] = calc_df.apply(lower_e,axis=1,level=0.95)*100
-    final_variant_df["upper_ci"] = calc_df.apply(upper_e,axis=1,level=0.95)*100
+    final_variant_df["lower_ci"] = calc_df.apply(calc_error,axis=1,mode="lower",level=0.95)*100
+    final_variant_df["upper_ci"] = calc_df.apply(calc_error,axis=1,mode="upper",level=0.95)*100
 
-
-    save_dir = "data/variant_data.csv"
-
-    final_variant_df.to_csv(save_dir)
-    ## droplet_cts=calc_df.iloc[93]
-    # r_hat(droplet_cts)
-    # calc_df.apply(loglik_trinom_prof,axis=1,ratio=r_hat(droplet_cts))
-    # #
-    # lower_e(droplet_cts)
-    # upper_e(droplet_cts)
-
-
+    final_variant_df.to_csv("data/variant_data.csv")
 
     gsheet = gc.open("COWWID LAB SHEET")
     worksheet = gsheet.worksheet("variant_monitoring")
     worksheet.clear()
 
     gsdf.set_with_dataframe(worksheet, final_variant_df, include_index=True)
-
-    # plot_df = final_variant_df.reset_index().copy()
-    # plot_df["sample_date"]= pd.to_datetime(plot_df["sample_date"])
-
-    # fig, axs = plt.subplots(2,3, figsize=(14,9), sharex=True, sharey=True)
-    # plt.style.use('dark_background')
-    # colors = ["k",'darkgray','blue','red','lightgreen','magenta','cyan','orange',"lavender"]
-    #
-    # for i,wwtp in enumerate(plot_df["wwtp"].unique()):
-    #
-    #
-    #     axis = axs.ravel()[i]
-    #     subplot_df = plot_df[plot_df['wwtp'] == wwtp].copy()
-    #
-    #     axis.set_title(wwtp, fontsize=10)
-    #     axis.set_xlabel("",fontsize=10)
-    #     axis.set_ylabel("",fontsize=10)
-    #
-    #     for tick in axis.get_xticklabels():
-    #         tick.set_rotation(45)
-    #     sns.lineplot(
-    #         x=subplot_df["sample_date"],
-    #         y=subplot_df["PercMutation"],
-    #         hue=subplot_df["target_variant"],
-    #         style=subplot_df["target_variant"],
-    #         palette="hsv",
-    #         markers=True,
-    #         alpha=0.8,
-    #         ax=axis
-    #     )
-    #
-    #     for variant in subplot_df["target_variant"].unique():
-    #         variant_df = subplot_df[subplot_df["target_variant"] == variant]
-    #         axis.fill_between(
-    #             x=variant_df["sample_date"],
-    #             y1=variant_df["upper_ci"],
-    #             y2=variant_df["lower_ci"],
-    #             fc="white",
-    #             lw=0,
-    #             alpha=0.3,
-    #         )
-    #
-    # plt.tight_layout()
-    # plt.show()
-    #
-    #
-    # os.chdir("/Volumes/KatahdinHD/ResilioSync/DATA/pydata/cowwid/pycode")
-
-# def fix_later():
-    # confint_df = variant_df[["NegativeDroplets", "FAM_single_PositiveDroplets","HEX_single_PositiveDroplets","double_PositiveDroplets"]]
-    #
-    # confint_df.apply(r_hat,axis=1)
-    #
-    #
-    # confint_df.apply(loglik_trinom_prof,axis=1,ratio=1)
-    #
-    # confint_df.apply(lower_e,axis=1)
-
-
-
-    # print(r_hat(val))
-    # print(loglik_trinom_prof(val,ratio=0))
-    # print(loglik_trinom_prof(val,ratio=r_hat(val)))
-# final_df = variant_df[droplet_cols].copy()
-    # test_df = variant_df[["SampleName"]+droplet_cols].dropna().reset_index(drop=True)
-# with pd.ExcelWriter("VariantResults.xlsx", date_format="YYYY-MM-DD", engine="openpyxl") as writer:
-#     final_df.to_excel(writer, sheet_name="Results", index=True)
-    # test_df["DoubleNegDroplets"] = test_df["TotalDroplets"] - test_df[droplet_cols[1:]].sum(axis=1)
-    # x=test_df[["DoubleNegDroplets","FAM_single_pos._PositiveDroplets","double_pos._PositiveDroplets"]]
-
-
-
-
-
-
-
-
-
-
-
-##------------------------------------------------------------------------------------------------##
-
-
-##------------------------------------------------------------------------------------------------##
-
-##------------------------------------------------------------------------------------------------##
-
-##------------------------------------------------------------------------------------------------##
